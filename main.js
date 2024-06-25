0440a31ae8fc3453106d6afa8938087f90da9d58
peterg <97988826+pedrogalvis@users.noreply.github.com>
2024-06-25 10:42:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Pedir al usuario que ingrese el número//
+//let input = parseInt(prompt("Digite el número entero para crear la tabla de multiplicar: "));
+
+// Para introducir los encabezados de la tabla
+//let resultado = "<tr><th>Multiplicación</th><th>Resultado</th></tr>";
+
+// Ciclo o bucle para que se repita una acción. Multiplicar 12 veces el número dado por el usuario.
+//for (let i = 1; i <= 12; i++) {
+    resultado += `<tr><td>${input} X ${i} =</td><td>${input * i}</td></tr>`;
+
+
+// Mostrar en pantalla el resultado
+//document.getElementById('tabla').innerHTML = resultado;
